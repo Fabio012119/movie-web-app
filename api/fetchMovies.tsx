5eea6171575b1ae69ddd4866e1329a0521f26d3b
Fabio <fabiooosara@gmail.com>
2025-05-30 10:41:01 +0300

    some fmt

--- api/fetchMovies.tsx
+++ api/fetchMovies.tsx
@@ -3,19 +3,18 @@ import type { queryType } from "@/types/general";
 
 export async function fetchMovies<T>(
   endpoint: string,
-  query: queryType = {},
+  query: queryType = {}
 ): Promise<T> {
   const params = new URLSearchParams({
     language: "en-US",
     ...Object.fromEntries(
-      Object.entries(query).map(([k, v]) => [k, String(v)]),
+      Object.entries(query).map(([k, v]) => [k, String(v)])
     ),
   });
 
   const res = await fetch(`${TMDB_BASE_URL}/${endpoint}?${params}`, {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmE3NTA4MTA3NTkxYTllMDhjNDJjYzQ1ZTBiMWMwZSIsIm5iZiI6MTY0Njc3MTQ2OS4yNDYsInN1YiI6IjYyMjdiZDBkMTEzMGJkMDA2N2MzYzA4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3hlVKMa4vy-ASLSurfzpNyMe0FwjyNCHAVBQ5x3VBa0
-`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmE3NTA4MTA3NTkxYTllMDhjNDJjYzQ1ZTBiMWMwZSIsIm5iZiI6MTY0Njc3MTQ2OS4yNDYsInN1YiI6IjYyMjdiZDBkMTEzMGJkMDA2N2MzYzA4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3hlVKMa4vy-ASLSurfzpNyMe0FwjyNCHAVBQ5x3VBa0`,
     },
   });
   if (!res.ok) throw new Error("Failed to fetch from TMDb");
